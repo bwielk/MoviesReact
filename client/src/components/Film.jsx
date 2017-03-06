6ba64234d8a8a6625610c573844fce09866282c6
Blazej <bwielk@gmail.com>
2017-03-06 23:46:56 +0000

    Added buttons

--- client/src/components/Film.jsx
+++ client/src/components/Film.jsx
@@ -8,7 +8,7 @@ var Film = React.createClass({
       <ul>
       <li>
       <a href = {this.props.url}><h4>{this.props.title}</h4></a>
-      <a href = {this.props.showtime}></a>
+      <button type = "button" onClick = {this.props.showtime}> See showtimes</button>
       </li>
       </ul>
       </div>
